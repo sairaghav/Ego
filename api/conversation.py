643cf0e00d83b4501f9706f36174fd0a5ae7c9f2
unknown <v.sairaghav@gmail.com>
2018-01-17 19:49:34 +0530

    Integrated conversation with command mode for better experience

--- api/conversation.py
+++ api/conversation.py
@@ -1,22 +1,28 @@
 import speaker,listener,google_search
 
-def converse():
-    conversation = 1
-    speaker.speak('What would you like to talk about?')
-    
-    while conversation == 1:
-        data = listener.listen()
+def get_response(data):
+    answer = google_search.get_summary(data)
 
-        if 'command mode' in data:
-            conversation = 0
-            speaker.speak('It was nice talking to you. See you soon!')
+    if (answer is None or answer == '') and not data is '':
+        speaker.speak('Sorry.. I don\'t know anything about that. Can we talk something else?')
+    elif (answer is None or answer == '') and data is '':
+        pass
+    else:
+        speaker.speak(answer)
+
+def converse(data='$'):
+    if data == '$':
+        conversation = 1
+        speaker.speak('What would you like to talk about?')
         
-        else:
-            answer = google_search.get_summary(data)
+        while conversation == 1:
+            data = listener.listen()
 
-            if answer is None and not data is '':
-                speaker.speak('Sorry.. I don\'t know anything about that. Can we talk something else?')
-            elif answer is None and data is '':
-                pass
+            if 'command mode' in data:
+                conversation = 0
+                speaker.speak('It was nice talking to you. See you soon!')
+            
             else:
-                speaker.speak(answer)
+                get_response(data)
+    else:
+        get_response(data)
---
+++ ego_v2.1/ego_v2.1.py
@@ -0,0 +1,84 @@
+import sys, time, threading
+sys.path.append('../')
+from api import speaker,listener,browser,process_handler,note_taker,window_switcher,key_presser,volume_controller,google_search,conversation,reader
+
+def get_input():
+    speak_data = listener.listen()
+    while speak_data == '':
+            speaker.speak('Sorry.. Can you say that again??')
+            speak_data = listener.listen()
+    return speak_data
+    
+
+def ego(data):
+    global listening
+
+    if listening == 1:
+        if data in ['search']:
+            speaker.speak('What do you want me to search for??')
+            query = get_input()
+
+            speaker.speak('Searching for '+query)
+            threading.Thread(target=browser.browse,args=('https://www.google.com/search?q='+query,)).start()
+
+        elif data in ['play music']:
+            speaker.speak('Which song do you want me to play??')
+            song = get_input()
+
+            speaker.speak('Playing '+song)
+            threading.Thread(target=browser.browse,args=(google_search.search_video(song)[0],)).start()
+            
+        elif 'switch window' in data:
+            window_switcher.switch_windows()
+
+        elif 'press' in data:
+            key_presser.press_key(data)
+            
+        elif data in ['show desktop','close window','close tab','new tab','restore tab']:
+            key_presser.press_key(data)
+
+        elif data in ['take notes']:
+            note_taker.take_notes()
+
+        elif 'volume' in data:
+            volume_controller.controller(data)
+
+        elif data in ['read news']:
+            reader.read_news()
+
+        elif 'read book' in data:
+            reader.read_book()
+
+        elif 'go to sleep' in data:
+            speaker.speak('Going to sleep..')
+            listening = 0
+
+        elif 'exit' in data:
+            speaker.speak('Exiting... Goodbye!')
+            listening = 0
+            sys.exit(0)
+
+        else:
+            conversation.converse(data)
+            
+    if data in ['hi ego', 'wake up ego', 'hey ego', 'wake up', 'ego']:
+        speaker.speak('Hello! I am Ego.. What can I do for you??')
+        listening = 1
+
+    elif 'exit' in data:
+        speaker.speak('Exiting... Goodbye!')
+        sys.exit(0)
+
+    
+
+if __name__ == '__main__':
+    try:
+        speaker.speak('Hello! I am Ego.. What can I do for you??')
+        listening = 1
+        
+        while True:
+            data = listener.listen()
+            ego(data)
+
+    except KeyboardInterrupt:
+        sys.exit(0)
