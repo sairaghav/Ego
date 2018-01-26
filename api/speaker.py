e8ce89af1510e4a8bf723c2cff78db14d90c3e0b
unknown <v.sairaghav@gmail.com>
2018-01-26 14:40:22 +0530

    Slowed down speaker speed

--- api/speaker.py
+++ api/speaker.py
@@ -2,11 +2,13 @@ import pyttsx,sys
 def speak(text):
     try:
         engine = pyttsx.init()
+        engine.setProperty('rate',150)
+
         voices = engine.getProperty('voices')
         if len(voices) > 1:
             engine.setProperty('voice', voices[1].id)
 
         engine.say(text)
         engine.runAndWait()
-    except KeyboardInterrupt:
+    except:
         sys.exit(0)
